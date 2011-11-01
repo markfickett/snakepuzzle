61bc9623d73653bf6020713397b941e4c7a30a91
Mark Fickett <mark.fickett@gmail.com>
2011-11-01 00:52:33 -0400

    Working solver.

--- snakepuzzle.py
+++ snakepuzzle.py
@@ -5,7 +5,7 @@ and solves their arrangement.
 
 Pseudocode:
 
-Start with a pile of pieces (two ends, some corners, some straights), and an
+Start with a pile of pieces (two ends, 16 corners, 9 straights), and an
 empty volume (a 3x3 cube).
 
 Take an end ends and put it (not facing an outside) in the cube.
@@ -29,64 +29,201 @@ Try another location/piece matching:
 """
 
 
-ends = two ends
-corners = some corners
-straights = some straights
-volume = 3x3 cube
-
-any piece can face (these are opposites):
-	down, up
-	left, right
-	front, back
-an end can either face (connect) to one direction, or from one direction
-a straight or a corner can face (connect) from any direction
-a straight always faces (connects) to the opposite
-	of the direction it faces (connects) from
-a corner can face (connect) to any direction
-	except the direction it faces (connects) from, or the opposite ove that
+from enum import Enum
 
 
-place a piece at a location and search from there:
-	put the piece at the location
-	if there was a piece before this one,
-		face this piece from the opposite of the direction
-		that the last piece was faced to
-	if the cube is now full,
-		save that solution
-	else try each of the directions the new piece can face towards:
-		try each of the remaining types of pieces:
-			figure out the location the next piece goes at
-			place the next piece there and search from there
-			then remove the next piece again
+Direction = Enum('down', 'up', 'left', 'right', 'front', 'back')
 
 
-place the first end:
-	face it towards the first direction
-	as long as it's facing towards the outside of the cube,
-		face it towards the next direction
+def GetOpposite(direction):
+	pairIndex = direction.index
+	inPairIndex = (1, 0)[direction.index % 2]
+	return Direction[2*(pairIndex/2) + inPairIndex]
 
 
-figure out the location the next piece goes at:
-	find the location of the previous piece
-	find the direction the previous piece is facing (connecting) to
-	this determines the location of the next piece
+class Piece():
+	def __init__(self):
+		self.faceFrom = None
+		self.faceTo = None
+		self.location = None
+		self.next = None
+
+	def __str__(self):
+		return '%s %s %s' % (self.LETTER, self.faceTo, self.location)
+
+class End(Piece):
+	LETTER = 'E'
+	def getFaceToPossibilities(self):
+		if self.faceFrom is not None:
+			raise RuntimeError('End has a faceFrom, cannot faceTo.')
+		return list(Direction)
 
 
-each of the remaining types of pieces:
-	if we have corners,
-		include a corner
-	if we have straights,
-		include a straight
-	if we had neither,
-		we should have the other end left
-		so use the end
+class Corner(Piece):
+	LETTER = 'C'
+	def getFaceToPossibilities(self):
+		faceTos = set(Direction)
+		faceTos.remove(self.faceFrom)
+		faceTos.remove(GetOpposite(self.faceFrom))
+		return list(faceTos)
 
 
-to start:
-	use the first end
-	at each location in the cube,
-		place the first end at that location, and search from there,
-		keeping track of all the solutions we save along the way
-	then print out all the solutions
+class Straight(Piece):
+	LETTER = 'S'
+	def getFaceToPossibilities(self):
+		return [GetOpposite(self.faceFrom)]
+
+
+class Cube:
+	def __init__(self):
+		self.start = None
+		self.__availables = {}
+		for i in xrange(3):
+			for j in xrange(3):
+				for k in xrange(3):
+					self.__availables[(i,j,k)] = True
+
+	def getUniqueStartingLocations(self):
+		return (
+			(0, 0, 0),	# a corner
+			(1, 0, 0),	# a middle of an edge
+			(1, 1, 0),	# a middle of a face
+			(1, 1, 1),	# the middle of the cube
+		)
+
+	def isAvailable(self, location):
+		return self.__availables.get(location, False)
+
+	def takeLocation(self, location):
+		if not self.isAvailable(location):
+			raise RuntimeError(
+				'Location not available to be taken: %s.'
+				% location)
+		self.__availables[location] = False
+
+	def releaseLocation(self, location):
+		status = self.__availables.get(location)
+		if status is None:
+			raise RuntimeError(
+				'Location not in volume to be released: %s.'
+				% location)
+		elif status == True:
+			raise RuntimeError(
+				'Location not taken to be released: %s.'
+				% location)
+		self.__availables[location] = True
+
+	def isFilled(self):
+		# TODO easy optimization waiting
+		return not any(self.__availables.values())
+
+	def getSolution(self):
+		if not self.isFilled():
+			raise RuntimeError(
+				'Volume not filled! No present solution.')
+		piece = self.first
+		strPieces = []
+		while piece:
+			strPieces.append(str(piece))
+			piece = piece.next
+		return ', '.join(strPieces)
+
+
+ends = [End() for i in xrange(2)]
+corners = [Corner() for i in xrange(16)]
+straights = [Straight() for i in xrange(9)]
+volume = Cube()
+
+
+def PlacePieceAndSearch(volume, prevPiece, piece, location,
+		ends, corners, straights):
+	if not volume.isAvailable(location):
+		return
+	volume.takeLocation(location)
+	piece.location = location
+	if prevPiece is not None:
+		piece.faceFrom = GetOpposite(prevPiece.faceTo)
+		prevPiece.next = piece
+	if volume.isFilled():
+		yield volume.getSolution()
+	else:
+		for faceTo in piece.getFaceToPossibilities():
+			piece.faceTo = faceTo
+			for solution in TryMorePieces(volume, piece,
+					ends, corners, straights):
+				yield solution
+	volume.releaseLocation(location)
+	if prevPiece is not None:
+		prevPiece.next = None
+
+
+def TryMorePieces(volume, piece, ends, corners, straights):
+	if not any([ends, corners, straights]):
+		raise RuntimeError('Need to try more pieces, but none left!')
+	nextLocation = CalculateNextLocation(piece)
+	if corners:
+		nextCorner = corners.pop()
+		for solution in PlacePieceAndSearch(
+				volume, piece, nextCorner, nextLocation,
+				ends, corners, straights):
+			yield solution
+		corners.append(nextCorner)
+	if straights:
+		nextStraight = straights.pop()
+		for solution in PlacePieceAndSearch(
+				volume, piece, nextStraight, nextLocation,
+				ends, corners, straights):
+			yield solution
+		straights.append(nextStraight)
+	if not any([corners, straights]):
+		finalEnd = ends.pop()
+		for solution in PlacePieceAndSearch(
+				volume, piece, finalEnd, nextLocation,
+				ends, corners, straights):
+			yield solution
+		ends.append(finalEnd)
+
+
+def CalculateNextLocation(piece):
+	"""
+	X right, Y up, Z front
+	"""
+	location = piece.location
+	if location is None:
+		raise RuntimeError(
+			'Cannot get next location, piece has no location.')
+	if piece.faceTo is None:
+		raise RuntimeError(
+			'Cannot get next location, piece has no faceTo.')
+	x, y, z = location
+
+	if piece.faceTo == Direction.down:
+		return (x, y-1, z)
+	elif piece.faceTo == Direction.up:
+		return (x, y+1, z)
+	elif piece.faceTo == Direction.left:
+		return (x-1, y, z)
+	elif piece.faceTo == Direction.right:
+		return (x+1, y, z)
+	elif piece.faceTo == Direction.back:
+		return (x, y, z-1)
+	else:
+		return (x, y, z+1)
+
+
+def PrintOpposites():
+	for d in Direction:
+		print d
+		print '\t', GetOpposite(d)
+
+
+if __name__ == '__main__':
+	firstEnd = ends.pop()
+	volume.first = firstEnd
+	for location in volume.getUniqueStartingLocations():
+		for solution in PlacePieceAndSearch(
+				volume, None, firstEnd, location,
+				ends, corners, straights):
+			print solution
 
 
